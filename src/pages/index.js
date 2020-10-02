dfbd3cf1c37f72248b327acc7df97a79028e079b
Meagan Butters <meagan.butters@hotmail.com>
2020-10-02 13:30:03 -0300

    Started about page - added SVGs without resize

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,13 @@
 import React from "react"
 import Layout from "../components/common/layout"
 import Intro from "../components/landing/intro/index"
+import About from "../components/landing/about/index"
 
 export default function Home() {
   return (
     <Layout>
       <Intro />
+      <About />      
     </Layout>
   )
 }
