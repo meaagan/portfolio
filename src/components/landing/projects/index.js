9bf4b5ef5d8e547adacb5374fad5135fa607baae
Meagan Butters <meagan.butters@hotmail.com>
2020-11-03 17:07:12 -0500

    Added fade in

--- src/components/landing/projects/index.js
+++ src/components/landing/projects/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import styles from "./index.module.css";
+import Fade from 'react-reveal/Fade';
 
 import { useStaticQuery, graphql } from "gatsby";
 import Img from "gatsby-image";
@@ -34,46 +35,52 @@ const Projects = () => {
       return (       
         <div id='projects' className={styles.projects}>
           <h1 style={{textAlign:"center"}}>Projects</h1>
-          <div className={styles.project}>
-            <figure className={styles.projectImg}>
-              <Img fluid = {data.cheers.childImageSharp.fluid} />
-            </figure>
-            <div className={styles.projectText}>
-              <p>
-                Cheers! is a cocktail database where a user can view detailed information on any cocktail, organized alphabetically. It was built with Ruby on Rails with help from <a href="https://www.thecocktaildb.com/">The Cocktail DB</a> and is hosted on Heroku.
+          <Fade>
+            <div className={styles.project}>
+              <figure className={styles.projectImg}>
+                <Img fluid = {data.cheers.childImageSharp.fluid} />
+              </figure>
+              <div className={styles.projectText}>
                 <p>
-                  <a href="http://cocktail-database.herokuapp.com/">View Cheers!</a></p>
-              </p>
-            </div>
-        </div>
-        <div className={styles.project}>
-          <figure className={styles.projectImg}>
-            <Img fluid = {data.rentie.childImageSharp.fluid} />
-          </figure>
-          <div className={styles.projectText}>
-              <p>Rentie is a property management tool for small to medium property owners. It was built with Ruby on Rails, Javascript 2015, PostgreSQL, ChartJS, and SCSS, by Kyle Masterson, Francesca Hall, Millie Senecal, and Meagan Butters and is hosted on Heroku.
-                <p><a href="http://www.rentie.ca/">View Rentie</a></p>
-                <p>
-                  Please use the following login information: 
+                  Cheers! is a cocktail database where a user can view detailed information on any cocktail, organized alphabetically. It was built with Ruby on Rails with help from <a href="https://www.thecocktaildb.com/">The Cocktail DB</a> and is hosted on Heroku.
+                  <p>
+                    <a href="http://cocktail-database.herokuapp.com/">View Cheers!</a></p>
                 </p>
-                e-mail: meagan@email.com, password: 123456
-              </p>
+              </div>
             </div>
-        </div>
-          <div className={styles.project}>
-            <figure className={styles.projectImg}>
-              <Img fluid = {data.melody.childImageSharp.fluid} />          
-            </figure>
-            <div className={styles.projectText}>
-                Melody is an Airbnb clone for instruments. It was built with Ruby on Rails, Javascript 2015, PostgreSQL, Algolia, AJAX, and SCSS, by Kyle Masterson, Francesca Hall, Millie Senecal, and Meagan Butters and is hosted on Heroku.
-                <p>
-                  <a href="http://airbnb-meaagan.herokuapp.com/">View Melody</a></p>
-                <p>
-                  Please use the following login information: 
-                </p>
-                e-mail: meagan@email.com, password: 123456
+          </Fade>
+          <Fade>
+            <div className={styles.project}>
+              <figure className={styles.projectImg}>
+                <Img fluid = {data.rentie.childImageSharp.fluid} />
+              </figure>
+              <div className={styles.projectText}>
+                  <p>Rentie is a property management tool for small to medium property owners. It was built with Ruby on Rails, Javascript 2015, PostgreSQL, ChartJS, and SCSS, by Kyle Masterson, Francesca Hall, Millie Senecal, and Meagan Butters and is hosted on Heroku.
+                    <p><a href="http://www.rentie.ca/">View Rentie</a></p>
+                    <p>
+                      Please use the following login information: 
+                    </p>
+                    e-mail: meagan@email.com, password: 123456
+                  </p>
+                </div>
             </div>
-          </div>
+          </Fade>
+          <Fade>
+            <div className={styles.project}>
+              <figure className={styles.projectImg}>
+                <Img fluid = {data.melody.childImageSharp.fluid} />          
+              </figure>
+              <div className={styles.projectText}>
+                  Melody is an Airbnb clone for instruments. It was built with Ruby on Rails, Javascript 2015, PostgreSQL, Algolia, AJAX, and SCSS, by Kyle Masterson, Francesca Hall, Millie Senecal, and Meagan Butters and is hosted on Heroku.
+                  <p>
+                    <a href="http://airbnb-meaagan.herokuapp.com/">View Melody</a></p>
+                  <p>
+                    Please use the following login information: 
+                  </p>
+                  e-mail: meagan@email.com, password: 123456
+              </div>
+            </div>
+          </Fade>
       </div>
     )
 }
