247575131cb21342e4110a13c6dd8f6f791a8a2c
Meagan Butters <meagan.butters@hotmail.com>
2020-11-21 14:50:14 -0500

    Added new projects

--- src/components/landing/projects/index.js
+++ src/components/landing/projects/index.js
@@ -41,6 +41,7 @@ const Projects = () => {
                 <Img fluid = {data.cheers.childImageSharp.fluid} />
               </figure>
               <div className={styles.projectText}>
+                <h2>Cheers!</h2>
                 <p>
                   Cheers! is a cocktail database where a user can view detailed information on any cocktail, organized alphabetically. It was built with Ruby on Rails with help from <a href="https://www.thecocktaildb.com/">The Cocktail DB</a> and is hosted on Heroku.
                   <p>
@@ -52,9 +53,40 @@ const Projects = () => {
           <Fade>
             <div className={styles.project}>
               <figure className={styles.projectImg}>
+                <Img fluid = {data.cheers.childImageSharp.fluid} />
+              </figure>
+              <div className={styles.projectText}>
+                <h2>Julian Nasielski Portfolio</h2>
+                <p>
+                  A portfolio for level designer, Julian Nasielski. Simple CMS-based website built with Gatsby, Prismic, and Netlify.
+                <p>
+                  <a href="http://www.juliannasielski.com/">View Julian Nasielski Portfolio</a></p>
+                </p>
+              </div>
+            </div>
+          </Fade>
+          <Fade>
+            <div className={styles.project}>
+              <figure className={styles.projectImg}>
+                <Img fluid = {data.cheers.childImageSharp.fluid} />
+              </figure>
+              <div className={styles.projectText}>
+                <h2>Victor Rose Espresso Bar</h2>
+                <p>
+                  Landing page for Victor Rose Espresso Bar. Made with Gatsby and hosted on Netlify.
+                <p>
+                  <a href="http://www.cafevictorrose.com/">View Victor Rose Espresso Bar</a></p>
+                </p>
+              </div>
+            </div>
+          </Fade>
+          <Fade>
+            <div className={styles.project}>
+              <figure className={styles.projectImg}>
                 <Img fluid = {data.rentie.childImageSharp.fluid} />
               </figure>
               <div className={styles.projectText}>
+                <h2>Rentie</h2>
                   <p>Rentie is a property management tool for small to medium property owners. It was built with Ruby on Rails, Javascript 2015, PostgreSQL, ChartJS, and SCSS, by Kyle Masterson, Francesca Hall, Millie Senecal, and Meagan Butters and is hosted on Heroku.
                     <p><a href="http://www.rentie.ca/">View Rentie</a></p>
                     <p>
@@ -71,7 +103,8 @@ const Projects = () => {
                 <Img fluid = {data.melody.childImageSharp.fluid} />          
               </figure>
               <div className={styles.projectText}>
-                  Melody is an Airbnb clone for instruments. It was built with Ruby on Rails, Javascript 2015, PostgreSQL, Algolia, AJAX, and SCSS, by Kyle Masterson, Francesca Hall, Millie Senecal, and Meagan Butters and is hosted on Heroku.
+                <h2>Melody</h2>
+                  <p>Melody is an Airbnb clone for instruments. It was built with Ruby on Rails, Javascript 2015, PostgreSQL, Algolia, AJAX, and SCSS, by Kyle Masterson, Francesca Hall, Millie Senecal, and Meagan Butters and is hosted on Heroku.</p>
                   <p>
                     <a href="http://airbnb-meaagan.herokuapp.com/">View Melody</a></p>
                   <p>
